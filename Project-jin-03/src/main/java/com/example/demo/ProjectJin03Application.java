c40220bf7146a89e0d8e7a9a0f934a0ffc4909fc
jinjnnho <junho11_kr@naver.com>
2024-05-22 13:09:54 +0900

    24.05.22

--- Project-jin-03/src/main/java/com/example/demo/ProjectJin03Application.java
+++ Project-jin-03/src/main/java/com/example/demo/ProjectJin03Application.java
@@ -2,7 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// JSA의 리스너 기능을 활성화
+// 데이터가 생성 및 수정되는 것을 추적
+@EnableJpaAuditing
 @SpringBootApplication
 public class ProjectJin03Application {
 
